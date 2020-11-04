8e9140fe7742c34f0a77889312d2a7512fcf4517
sai_yuki <cai_yuki@hotmail.com>
2020-11-04 11:33:22 +0900

    2020/11/04 Add notification system (only tested in windows)

--- client/testModule/clipboard_monitor.py
+++ client/testModule/clipboard_monitor.py
@@ -4,6 +4,7 @@ sys.path.append(os.path.abspath(".."))
 from helperModule import helper
 
 import pyperclip
+from plyer import notification
 
 def monitor():
     previous = pyperclip.paste()
@@ -14,5 +15,12 @@ def monitor():
             if now != previous:
                 previous = now
                 helper.send_str("This guy is copying something!")      
+                notification.notify(
+                    title="Suspicious action detected!",
+                    message="Contents in clipboard changed",
+                    app_name="Vega",
+                    app_icon="./vega.ico",
+                    timeout=3
+                )
         except:
             break
