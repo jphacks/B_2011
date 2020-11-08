c43420eaf7794a00e3032d6beefaf93643432bf7
bentaro <swd.ben@gmail.com>
2020-11-08 21:57:03 +0900

    removed unnecessary files

--- server/frontend/src/config/Status.ts
+++
@@ -1,4 +0,0 @@
-export default {
-  INITIALIZE: 0,
-  LOADED: 1
-}
--- server/frontend/vue.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  transpileDependencies: [
-    'vuetify'
-  ],
-  devServer: {
-    headers: { "Access-Control-Allow-Origin": "*" }
-  }
-}
