2c7b5ae4e809b42d39266e87fe5436b310c0b6c1
bentaro <swd.ben@gmail.com>
2020-11-09 16:03:31 +0900

    implemented websocket server modules

--- server/proctor/websocket/routing.py
+++ server/proctor/websocket/routing.py
@@ -2,5 +2,5 @@ from django.urls import path
 from websocket import consumers
 
 urlpatterns = [
-    path('ws/examinee/<str:room_name>', consumers.ExamineeConsumer.as_asgi()),
+    path('ws/examinee/<str:examinee_id>', consumers.ExamineeConsumer.as_asgi()),
 ]
