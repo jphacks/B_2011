567792f9114849a5bde6ae2c20b37f666d72477f
yuasabe <yuasabe@g.ecc.u-tokyo.ac.jp>
2020-11-21 12:44:17 +0900

    Change main window size

--- client/testModule/electron/src/main/main.js
+++ client/testModule/electron/src/main/main.js
@@ -31,7 +31,7 @@ app.setAppUserModelId("com.electron.vega")
 
 function createWindow() {
     const win = new BrowserWindow({
-        width: 800,
+        width: 1200,
         height: 600,
         webPreferences: {
             nodeIntegration: true
