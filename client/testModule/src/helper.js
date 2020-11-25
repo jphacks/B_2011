512125c81c7d1b6506843a94d933e48270319d30
yuasabe <yuasabe@g.ecc.u-tokyo.ac.jp>
2020-11-25 21:47:46 +0900

    Update helper

--- client/testModule/src/helper.js
+++ client/testModule/src/helper.js
@@ -2,9 +2,19 @@
 // Helper functions for SATORI Desktop App.
 
 const http = require('http')
+const { ipcRenderer } = require('electron')
 
-function send_json(user_id, exam_id, module_name, description, content) {
+function send_json(module_name, description, content) {
     
+    let exam_id = '911f6e61-e061-4be9-9f25-246e1fb16207';
+    let user_id = '911f6e61-e061-4be9-9f25-246e1fb16207'
+
+    ipcRenderer.send('get_user_data');
+    ipcRenderer.on('get_user_data', (event, data) => {
+        exam_id = data.exam_id
+        user_id = data.user_id
+    });
+
     const data = JSON.stringify([{
         "examinee_id": user_id,
         "exam_id": exam_id,
