250064c193988d9ffb8b222136bc7fe2f38c2d57
Koichiro Kanaya <abcs516tk1@gmail.com>
2020-11-26 12:07:53 +0900

    using mfcc to identify examinee's voice

--- client/testModule/voice/voice_anomaly_detection.py
+++ client/testModule/voice/voice_anomaly_detection.py
@@ -1,132 +1,243 @@
-import datetime
+import numpy as np
+import matplotlib.pyplot as plt
+import soundfile as sf
 import os
+import subprocess
 import shutil
 import pyaudio
 import wave
-import requests
-import json
-import numpy as np
-import sys
-sys.path.append(os.path.abspath('../..'))
-from helperModule import helper
+import math
+import datetime
+import time
+from scipy.fftpack.realtransforms import dct
+from sklearn.svm import SVC 
 
-DEVICE_INDEX = 1        # デバイスによって異なるゾ〜
+DEVICE_INDEX = 14        # デバイスによって異なるゾ〜
 CHUNK = 1024
 FORMAT = pyaudio.paInt16 # 16bit
 CHANNELS = 1             # monaural
 RATE = 44100             # sampling frequency [Hz]
 
-threshold = 0.30         # 音量の閾値
+threshold = 0.25         # 音量の閾値
+nceps = 10
 
-def anyone_speaking():
-    p = pyaudio.PyAudio()
-
-    stream = p.open(format=FORMAT,
-                    channels=CHANNELS,
-                    rate=RATE,
-                    input=True,
-                    input_device_index = DEVICE_INDEX,
-                    frames_per_buffer=CHUNK)
+def anyone_speaking(p, stream):
     while True:
         data = stream.read(CHUNK)
         x = np.frombuffer(data, dtype="int16") / 32768.0
 
         if x.max() > threshold:
             print(x.max())
-            return True
+            return
 
-# def record():
-#     rec_time = 5 # record time [s]
-#     # TODO : test. あとで消す。
+# 波形を適当な長さに分割し、窓関数をかけてFFTを行う。
+# start, endは秒数(float ok)
+def fft(filename, start, end):
+    # 音声の読み込み
+    master, fs = sf.read(filename)
 
-#     now = datetime.datetime.now()
-#     filename = 'log_' + now.strftime('%Y%m%d_%H%M%S')
-#     log_path = './log/' + filename + '.wav'
+    x = master[int(start*fs) : int(end*fs)]
+    # ハミング窓をかける
+    hamming = np.hamming(len(x))
+    x = x * hamming
 
-#     # TODO : なぜかここで出力される原因の究明
-#     p = pyaudio.PyAudio()
+    # 振幅スペクトルを求める。
+    N = 16384 # FFTのサンプル数
+    spec = np.abs(np.fft.fft(x, N))[:N//2]
+    fscale = np.fft.fftfreq(N, d=1.0/fs)[:N//2]
 
-#     stream = p.open(format=FORMAT,
-#                     channels=CHANNELS,
-#                     rate=RATE,
-#                     input=True,
-#                     input_device_index = DEVICE_INDEX,
-#                     frames_per_buffer=CHUNK)
+    return fscale, spec, fs, N
 
-#     frames = []
-#     for i in range(0, int(RATE / CHUNK * rec_time)):
-#         data = stream.read(CHUNK)
-#         frames.append(data)
 
-#     stream.stop_stream()
-#     stream.close()
-#     p.terminate()
+# Hz to mel
+def hz2mel(f):
+    return 2595 * np.log(f / 700.0 + 1.0)
 
-#     wf = wave.open(log_path, 'wb')
-#     wf.setnchannels(CHANNELS)
-#     wf.setsampwidth(p.get_sample_size(FORMAT))
-#     wf.setframerate(RATE)
-#     wf.writeframes(b''.join(frames))
-#     wf.close()
+# mel to Hz
+def mel2hz(m):
+    return 700 * (np.exp(m / 2595) - 1.0)
 
-#     return log_path, filename
+# メルフィルタバンクの作成
+def melFilterBank(fs, N, numChannels = 20):
+    # ナイキスト周波数（Hz）
+    fmax = fs / 2
+    # ナイキスト周波数（mel）
+    melmax = hz2mel(fmax)
+    # 周波数インデックスの最大数
+    nmax = N // 2
+    # 周波数解像度（周波数インデックス1あたりのHz幅）
+    df = fs / N
+    # メル尺度における各フィルタの中心周波数を求める
+    dmel = melmax / (numChannels + 1)
+    melcenters = np.arange(1, numChannels + 1) * dmel
+    # 各フィルタの中心周波数をHzに変換
+    fcenters = mel2hz(melcenters)
+    # 各フィルタの中心周波数を周波数インデックスに変換
+    indexcenter = np.round(fcenters / df)
+    # 各フィルタの開始位置のインデックス
+    indexstart = np.hstack(([0], indexcenter[0:numChannels - 1]))
+    # 各フィルタの終了位置のインデックス
+    indexstop = np.hstack((indexcenter[1:numChannels], [nmax]))
+    filterbank = np.zeros((numChannels, nmax))
+    for c in range(0, numChannels):
+        # 三角フィルタの左の直線の傾きから点を求める
+        increment= 1.0 / (indexcenter[c] - indexstart[c])
+        for i in range(int(indexstart[c]), int(indexcenter[c])):
+            filterbank[c, i] = (i - indexstart[c]) * increment
+        # 三角フィルタの右の直線の傾きから点を求める
+        decrement = 1.0 / (indexstop[c] - indexcenter[c])
+        for i in range(int(indexcenter[c]), int(indexstop[c])):
+            filterbank[c, i] = 1.0 - ((i - indexcenter[c]) * decrement)
 
-# def get():
-#     # TODO : URLを指定する。
-#     url = 'http://demo.ben.hongo.wide.ad.jp:8000/api/message/list'
-#     module_name = "voice"
-#     alert = True
-#     description = "本人ではない人の声を検出しました。"
-#     content = ""
+    return filterbank, fcenters
 
-#     helper.send_json(module_name, alert, description, content)
+# 離散コサイン変換を行う。
+def disc_cos(mspec):
+    ceps = dct(10 * np.log10(mspec), type=2, norm="ortho", axis=-1)
+    mfcc = ceps[:nceps]
+    return mfcc
 
-def get():
-    # TODO : URLを指定する。
-    url = 'http://demo.ben.hongo.wide.ad.jp:8000/api/message/list'
-    module_name = "voice"
-    alert = True
-    description = "大きな音を検出しました。"
-    content = ""
+def normalization(vague_vowel):
+    square_sum = 0
+    for i in vague_vowel:
+        square_sum += i*i
+    norm = math.sqrt(square_sum)
+    for i in range(len(vague_vowel)):
+        vague_vowel[i] /= norm
+    return vague_vowel
 
-    helper.send_json(module_name, alert, description, content)
+def record(p, stream):
+    print('録音開始！')
+    rec_time = 5 # [sec]
 
-def anomaly_detection():
-    if os.path.exists('output'):
-        shutil.rmtree('output')
+    now = datetime.datetime.now()
+    filename = 'log_' + now.strftime('%Y%m%d_%H%M%S') + '.wav'
+    filepath = os.path.join('./log', filename)
+
+    frames = []
+    for i in range(0, int(RATE / CHUNK * rec_time)):
+        data = stream.read(CHUNK)
+        frames.append(data)
+
+    wf = wave.open(filepath, 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+    wf.writeframes(b"".join(frames))
+    wf.close()
+
+    return filepath
+
+# TODO : サポートベクターマシンでmfccの分類のためのモデル構築を行う。
+def svm_model(X_train, Y_train):
+    model = SVC(gamma = 'scale')
+    model.fit(X_train, Y_train)
+    return model
+
+def voice_inference(model, filename):
+    dic = {'0':0, '1':0, '2':0, '3':0}
+    fig = plt.figure()
+    for i in range(5):
+        vague_vowel = [0] * nceps
+        for j in range(10):
+            fscale, spec, fs, N = fft(filename, (i*5+j)*0.1, (i*5+j+1)*0.1)
+            filterbank, fcenters = melFilterBank(fs, N, numChannels=20)
+
+            # 振幅スペクトルにメルフィルタバンクを適用
+            mspec = np.dot(spec, filterbank.T)
+            mfcc = disc_cos(mspec)
+            for k in range(nceps):
+                vague_vowel[k] += mfcc[k]
+        X_test = np.array(normalization(vague_vowel[1:nceps]))
+        X_test = X_test.reshape(1,-1)
+        plt.scatter(X_test[0][0], X_test[0][1])
+        Y_pred = int(model.predict(X_test)[0])
+        dic[str(Y_pred)]+=1
+    Y_pred = max(dic, key=dic.get)
+    fig.savefig("shun.png")
+
+    print('predict : ' + str(Y_pred))
+    if str(Y_pred[0]) == '0':
+        print('OK!')
+    else:
+        print('NG!!!')
+    # Y_pred = model.predict(X_test)
+    # print(Y_pred)
+
+def make_wav(s):
+    print('母音の" ' + str(s) + ' "を5秒間発声してください。')
+    time.sleep(1)
+    filename = './wav/0_' + s + '.wav'
+    subprocess.run(['rec', '-c', '1', '-r', '44100', filename, 'trim', '0', '5'])
+
+# main関数のようなもの。
+def run():
+    # TODO : フロントで母音を言うように出してもらって、それを
+    # TODO : './wav/examinee_a.wav'のように保存してもらう。
+    # 現状、一時的にexaminee_a.wavをmake_wav()で作成する。
+    # TODO : make_wav()は後で消す。
     if os.path.exists('log'):
         shutil.rmtree('log')
     os.mkdir('log')
-    os.mkdir('output')
+    for s in ['a','i','u','e','o']:
+        make_wav(s)
 
-    # 以下は試験中フェーズ
+
+    X_train = np.array([])
+    Y_train = np.array([])
+    # 学習データの生成
+    dir_path = './wav'
+    for i in range(4): # 何人いるか
+        for j in range(5,25):
+            Y_train = np.append(Y_train, i)
+            vague_vowel = [0] * nceps
+            for s in ['a', 'i', 'u', 'e', 'o']: # 一人あたり何個の音声学習データがあるか
+                filename = os.path.join(dir_path, str(i) + '_' + s +'.wav')
+                fscale, spec, fs, N = fft(filename, 0.2*j, 0.2*(j+1))
+                filterbank, fcenters = melFilterBank(fs, N, numChannels=20)
+                # 振幅スペクトルにメルフィルタバンクを適用
+                mspec = np.dot(spec, filterbank.T)
+                mfcc = disc_cos(mspec)
+                for k in range(nceps):
+                    vague_vowel[k] += mfcc[k]
+            vague_vowel = normalization(vague_vowel[1:nceps])
+            X_train = np.append(X_train, vague_vowel)
+    X_train = X_train.reshape(-1, nceps-1)
+
+    fig = plt.figure()
+    for i, s in enumerate(['r','b','g','y']):
+        x = []
+        y = []
+        for j in range(20):
+            x.append(X_train[i*20+j][0])
+            y.append(X_train[i*20+j][1])
+        plt.scatter(x,y,c=s)
+    
+    fig.savefig("ikuno.png")
+        
+    
+    # TODO : 0-indexに本人の音声のケプストラムを入れたい。
+    model = svm_model(X_train, Y_train)
+
+    # ---------------------------------------------------------
+    # ここから下は実際に試験中に音声を認識するフェーズに入る。
+    
+    # TODO : 取得した音声に対して、音声主を予測する。
+    p = pyaudio.PyAudio()
+    stream = p.open(format = FORMAT,
+                    channels = CHANNELS,
+                    rate = RATE,
+                    input=True,
+                    output = True,
+                    input_device_index = DEVICE_INDEX,
+                    frames_per_buffer = CHUNK,
+                    )
+
     while True:
-        # TODO : 音量がしきい値を超えたら録音スタート
-        if anyone_speaking() == True:
-            get()
+        anyone_speaking(p, stream) # 話し始めると以下に進む。
 
-""" def anomaly_detection():
-    if os.path.exists('output'):
-        shutil.rmtree('output')
-    if os.path.exists('log'):
-        shutil.rmtree('log')
-    os.mkdir('log')
-    os.mkdir('output')
+        filename = record(p, stream)
+        voice_inference(model, filename)
 
-    # 以下は試験中フェーズ
-    while True:
-        # TODO : 音量がしきい値を超えたら録音スタート
-        if anyone_speaking() == True:
-            pass
 
-        # caution : log_pathは相対パス
-        # caution : log_filenameは、録音ファイルがAAA.wavならAAA
-        log_path, log_filename = record()
-        os.system('./dialogue-demo/sid/test.sh ./' + log_path + ' ./output/' + log_filename + '.txt')
-        with open('./output/' + log_filename + '.txt') as f:
-            s = f.read()
-            s = s[0]
-            # 暫定でkanayaの声でない場合アラートを出す。
-            if s != '1':
-                get()
- """
+run()
